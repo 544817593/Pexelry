f900dcdb4bc886ad5b93782a3cb3bb3c9ac3e4e0
zaceJin <zacechel@gmail.com>
2022-03-08 20:48:06 +0000

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -117,6 +117,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    babel: {
+      compact: true,
+    },
     configureWebpack: {
       optimization: {
         runtimeChunk: 'single',
