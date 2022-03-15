e736853d2cac74bbe658431f52a2bbb0454e7c08
zaceJin <zacechel@gmail.com>
2022-03-15 01:23:23 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,12 @@
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: '',
+  server: {
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    target: '',
+    timing: false
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'PEXELRY - PIC IT',
